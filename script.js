0cf4afb5c5ddb0e7bd8d4c6b7ba89d5a9c9395f8
Kaique Ramiro <kaiqueramiro10@gmail.com>
2026-06-11 16:10:10 -0300

    ta completo

---
+++ script.js
@@ -0,0 +1,10 @@
+function entrar() {
+
+    document.getElementById("tela1").style.display = "none";
+
+    document.getElementById("tela2").style.display = "block";
+
+    document.getElementById("musica").play();
+
+    document.getElementById("videoSurpresa").play();
+}
